4eb01dc7106d531568dd7b5cc54d9c2386f80800
Timilehin Arigbede <Harriemannie@gmail.com>
2022-01-31 15:07:28 +0100

    Add LInks to header

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { AppBar, Container, Toolbar, Typography, Link } from '@mui/material';
 import useStyles from '../utils/styles';
+import NextLink from 'next/link';
 
 export default function Layout({ children }) {
   const classes = useStyles();
@@ -13,7 +14,18 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Clonedwolf</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Clonedwolf</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <NextLink href="/cart" passHref>
+            <Link>Cart</Link>
+          </NextLink>
+          <NextLink href="/login" passHref>
+            <Link>Login</Link>
+          </NextLink>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
