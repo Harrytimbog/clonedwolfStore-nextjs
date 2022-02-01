d31befbe80c27c2f69c4e01246e39f426ca55bfc
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-01 15:27:32 +0100

    Get Products from API

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,8 +1,6 @@
-import { useRouter } from 'next/router';
 import React from 'react';
 import Layout from '../../components/Layout';
 import NextLink from 'next/link';
-import data from '../../utils/data';
 import {
   Button,
   Card,
@@ -14,12 +12,12 @@ import {
 } from '@mui/material';
 import useStyles from '../../utils/styles';
 import Image from 'next/image';
+import Product from './models/Product';
+import db from '../../utils/db';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+  const { product } = props;
   const classes = useStyles();
-  const router = useRouter();
-  const { slug } = router.query;
-  const product = data.products.find((a) => a.slug === slug);
   if (!product) {
     return <div>Product Not Found</div>;
   }
@@ -102,4 +100,18 @@ export default function ProductScreen() {
       </Grid>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
 }
