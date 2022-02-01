9801cede303caf18c86ae53aaff2103102ea8a77
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-01 11:22:11 +0100

    Create React Context and store

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import {
   AppBar,
@@ -9,11 +9,17 @@ import {
   createTheme,
   ThemeProvider,
   CssBaseline,
+  Switch,
 } from '@mui/material';
 import useStyles from '../utils/styles';
 import NextLink from 'next/link';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
 
 export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+
   const theme = createTheme({
     typography: {
       h1: {
@@ -28,7 +34,7 @@ export default function Layout({ title, description, children }) {
       },
     },
     palette: {
-      type: 'light',
+      mode: darkMode ? 'dark' : 'light',
       primary: {
         main: '#f0c000',
       },
@@ -39,6 +45,12 @@ export default function Layout({ title, description, children }) {
   });
 
   const classes = useStyles();
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
 
   return (
     <div>
@@ -58,12 +70,15 @@ export default function Layout({ title, description, children }) {
               </Link>
             </NextLink>
             <div className={classes.grow}></div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
-            </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
+            <div>
+              <Switch checked={darkMode} onChange={darkModeChangeHandler} />
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
           </Toolbar>
         </AppBar>
         <Container className={classes.main}>{children}</Container>
