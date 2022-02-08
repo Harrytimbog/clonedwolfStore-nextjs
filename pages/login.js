f8330c9752cc64ec0ddd29b4050d608733eb3ddb
Timilehin Arigbede <Harriemannie@gmail.com>
2022-02-08 21:17:16 +0100

    Create user Profile page

--- pages/login.js
+++ pages/login.js
@@ -16,6 +16,7 @@ import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import { Controller, useForm } from 'react-hook-form';
 import { useSnackbar } from 'notistack';
+import { getError } from '../utils/error';
 
 export default function Login() {
   const {
@@ -48,10 +49,7 @@ export default function Login() {
       Cookies.set('userInfo', JSON.stringify(data));
       router.push(redirect || '/');
     } catch (error) {
-      enqueueSnackbar(
-        error.response.data ? error.response.data.message : error.message,
-        { variant: 'error' }
-      );
+      enqueueSnackbar(getError(error), { variant: 'error' });
     }
   };
 
